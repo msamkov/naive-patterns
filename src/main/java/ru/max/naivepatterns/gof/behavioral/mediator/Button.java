17ca9ffab4377663e1f1abc3258b1f59852cd6cf
msamkov <maxim.samkov@russianpost.ru>
2022-09-07 12:36:03 +0300

    GOF Mediator pattern

---
+++ src/main/java/ru/max/naivepatterns/gof/behavioral/mediator/Button.java
@@ -0,0 +1,13 @@
+package ru.max.naivepatterns.gof.behavioral.mediator;
+
+public class Button {
+    private Mediator mediator;
+
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    public void press() {
+        this.mediator.press();
+    }
+}
---
+++ src/main/java/ru/max/naivepatterns/gof/behavioral/mediator/PowerSupplier.java
@@ -0,0 +1,11 @@
+package ru.max.naivepatterns.gof.behavioral.mediator;
+
+public class PowerSupplier {
+    public void turnOn() {
+        // implementation
+    }
+
+    public void turnOff() {
+        // implementation
+    }
+}
